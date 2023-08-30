b60d5ce68e33602d46aa5b2674abe4617a23f649
Jordan Lockhart <jordanlockhart556@gmail.com>
2023-08-29 20:42:43 -0700

    make it better

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, '../client', 'dist')));
+
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
